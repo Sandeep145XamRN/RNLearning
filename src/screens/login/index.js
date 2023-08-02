a70dfaf93fbca296291dca7e027eb6456503f572
Sandeep Singh <sandeep.s@orionesolutions.in>
2023-08-02 14:00:02 +0530

    API implementation using javascript fetch

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 import localStyles from './styles';
 import CommonModal from '../../components/commonModal';
+import styles from './styles';
 
 const Login = props => {
+  const [email, setEmail] = useState('');
+  useEffect(() => {}, []);
+
+  const loginAction = async () => {
+    var formdata = new FormData();
+    formdata.append('email', email);
+    formdata.append('password', 'test@123');
+    formdata.append('role', 'restaurant');
+    formdata.append('fcm_token', 'restaurant');
+    console.warn('formdata', formdata);
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      // redirect: 'follow',
+    };
+
+    fetch('https://2topadmindev.clienturls.com/api/login', requestOptions)
+      .then(response => {
+        console.log('response1', response);
+        return response.text();
+      })
+      .then(result => console.log('result', result))
+      .catch(error => console.log('error', error));
+  };
+
   return (
     <View>
-      <Text>Login</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={txt => setEmail(txt)}
+        placeholder="Enter your email"
+      />
+
+      <TextInput style={styles.input} placeholder="Enter your password" />
+
       <TouchableOpacity
         style={localStyles.button}
         onPress={() => {
           //props.navigation.navigate('Otp');
-          props.navigation.openDrawer();
+          // props.navigation.openDrawer();
+          loginAction();
         }}>
-        <Text>Touch Here</Text>
+        <Text>Login</Text>
       </TouchableOpacity>
       <CommonModal />
     </View>
--- src/screens/login/styles.js
+++ src/screens/login/styles.js
@@ -8,6 +8,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 25,
+    alignSelf:'center',
+    marginTop:10,
   },
+  input:{
+    height:50,
+    width:'90%',
+    borderWidth:1,
+    marginTop:10,
+    alignSelf:'center'
+  }
 });
 export default styles;
